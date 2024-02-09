c307f9c3eaf7e6a962a0dcf252b2963e8b067b2e
Alberto Helbig <albertohelbig@gmail.com>
2024-02-09 00:01:52 -0300

    refactor: adjust project images

--- src/components/Image/Image.tsx
+++ src/components/Image/Image.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import hikingMenImg from '../../assets/img-case/man-hiking-in-mountains.jpg'
+import hikingMenImg from '../../assets/img-case/man-hiking-in-mountains.png'
 
 export function InacessibleImage(): React.JSX.Element {
   return <img src={hikingMenImg} width={250} height={400} />
